478a9fa6768aacc296daecd43ddaa59bac76dd3f
rfathima786 <git@github.com>
2025-10-30 17:03:57 +0530

    back

--- Day4-LockFile/backend.tf
+++ Day4-LockFile/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "rayees-s3-staticfile-test"  # Replace with your S3 bucket name
     key            = "Day4-LockFile/terraform.tfstate"    # Path in the bucket to store the state file
-    region         = "us-east-1"     
+    region         = "us-east-1"    
+    use_lockfile   = true 
     
   }
 }
