3eaf2bd812b007cc6e447db74a86b77d7f460cb5
rfathima786 <git@github.com>
2025-10-30 16:29:29 +0530

    Day4

---
+++ Day4-LockFile/main.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "Ray-VPC"
+    }
+}
+resource "aws_instance" "Rayees" {
+    instance_type ="t3.micro"
+    ami = "ami-07860a2d7eb515d9a"
+    
+  
+}
